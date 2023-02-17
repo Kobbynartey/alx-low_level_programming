0955a8ec11447b8f35efb7aa1022a13184754681
Benny Goodman <bennygoodman53@gmail.com>
2023-02-17 14:08:43 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphatet in order
+ *
+ * Return: (0) after code is successful
+ */
+
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+{
+	putchar(c);
+}
+	putchar('\n');
+	return (0);
+}
