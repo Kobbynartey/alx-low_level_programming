e40fad8fa2b66ae4869ed598af72e92b9c17928a
Benny Goodman <bennygoodman53@gmail.com>
2023-02-16 17:37:45 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)     
-*/
+ */
 
 int main(void)
 {
@@ -13,8 +13,7 @@ int main(void)
 	long along;
 	long long alonglong;
 	float afloat;
- 
-	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+ 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
 	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
