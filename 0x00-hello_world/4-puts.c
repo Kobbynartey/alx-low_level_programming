db633c2a5275088852bafc7398fc5f47d2410e16
Benny Goodman <bennygoodman53@gmail.com>
2023-02-16 17:19:04 -0800

    hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
-	puts(Mystr);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
