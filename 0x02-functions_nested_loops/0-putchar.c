f318e0f5aff0f1765ee4ed0c12e04884cab96107
Benny Goodman <bennygoodman53@gmail.com>
2023-02-21 09:59:05 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: (0) when successful
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
+
