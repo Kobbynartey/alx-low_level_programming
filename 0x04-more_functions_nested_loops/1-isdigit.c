c55a7168de1915cbfbe36e601707f64937552bf1
Benny Goodman <bennygoodman53@gmail.com>
2023-02-24 08:02:24 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - prints digits from 0 through 9
+ * @c: is the digit.
+ * Return: 1 if c is a digit else return 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 || c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
