8805486f0c39c25eb765cdcf70a4f7314fe4c831
Benny Goodman <bennygoodman53@gmail.com>
2023-02-24 08:36:30 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 t0 9
+ * Return: 0 as success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+}
