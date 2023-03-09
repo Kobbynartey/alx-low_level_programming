5010182c0ac9663fbdf0ce67a6cf7aadff90aaa7
Benny Goodman <bennygoodman53@gmail.com>
2023-03-09 10:48:33 -0800

    put recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts_recursion - prints a string followed by a new line
- * @*S - character
+ * @s: character
  * Return: void
  */
 
@@ -10,10 +10,10 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		printf("\n");
+		putchar('\n');
 		return;
-    	}
-	printf("%c", *s);
-
-	_puts_rec(s + 1);
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
 }
+
