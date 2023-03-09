b0522a115eedcea568f7c87ba018acb46c64dc84
Benny Goodman <bennygoodman53@gmail.com>
2023-03-09 14:47:08 -0800

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,15 +12,16 @@
 int sqtRecursive(int start, int end, int m)
 {
 	long mid;
+
 	if (end >= start)
 	{
 		mid = start + (end - start) / 2;
-		if (mid *mid == m)
+		if (mid * mid == m)
 			return (mid);
 
-		if (mid *mid > m)
+		if (mid * mid > m)
 			return (sqtRecursive(start, mid - 1, m));
-		if (mid *mid < m)
+		if (mid * mid < m)
 			return (sqtRecursive(mid + 1, end, m));
 	}
 	return (-1);
