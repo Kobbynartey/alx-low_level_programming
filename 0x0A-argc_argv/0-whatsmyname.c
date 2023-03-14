fd447fe956a26c443a7c84a243cb70db5e8307cf
Benny Goodman <bennygoodman53@gmail.com>
2023-03-14 16:10:04 -0700

    create array

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name followed by new line
+ * Return: 0 as success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		_putchar(argv[0]);
+
+}
