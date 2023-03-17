b594e66dfd68a03b303dad48f9f2b10ffdc81bac
Benny Goodman <bennygoodman53@gmail.com>
2023-03-17 16:04:46 -0700

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size to be allocated
+ * Return: pointer if success or exit with 98
+ **/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	ptr = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
