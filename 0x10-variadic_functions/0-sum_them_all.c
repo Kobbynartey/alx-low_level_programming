cc1e9f95095deeba6e460601687318a3d8afa7d5
Benny Goodman <bennygoodman53@gmail.com>
2023-03-23 10:27:11 -0700

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,6 +3,7 @@
 /**
  * sum_them_all - function that sums all numbers given
  * @n : integer
+ * Return: sum if success or 0 if n is 0
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -11,14 +12,14 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(list, n);
 
-	int i, sum = 0;
+	unsigned int i, sum = 0;
 
 	if (n == 0)
 		return (0);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(list, const unsigned int);
+		sum += va_arg(list, int);
 	}
 
 	va_end(list);
