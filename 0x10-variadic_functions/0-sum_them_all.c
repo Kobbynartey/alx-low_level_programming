e463851f01aeaf553a768b3e1a7c3a9e0e086f00
Benny Goodman <bennygoodman53@gmail.com>
2023-03-23 10:47:31 -0700

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,12 +10,10 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i, sum = 0;
 	va_list list;
 
 	va_start(list, n);
-
-	unsigned int i;
-	int sum = 0;
 
 	if (n == 0)
 		return (0);
