39933c9fdd821ede62e2d16767c447dc34148938
Benny Goodman <bennygoodman53@gmail.com>
2023-04-05 07:55:49 -0700

    list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,11 +8,11 @@
 
 size_t print_listint(const listint_t *h)
 {
-	unsigned int nd;
+	unsigned int nd = 0;
 
 	while (h != NULL)
 	{
-		printf("%i\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		nd++;
 	}
